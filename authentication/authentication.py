2b7d1303efce791ea55d35f6219627d15217c033
Hesam Haghpanah <43572130+h-haghpanah@users.noreply.github.com>
2023-10-30 16:12:15 +0330

    put access and refresh token in django settings

--- authentication/authentication.py
+++ authentication/authentication.py
@@ -3,6 +3,7 @@ from rest_framework import exceptions
 from rest_framework.authentication import BaseAuthentication,get_authorization_header
 from .models import User
 from .views import UserSerializer
+from django.conf import settings
 
 class JWTAuthentication(BaseAuthentication):
     def authenticate(self,request):
@@ -19,11 +20,11 @@ def create_access_token(id):
         'user_id':id,
         'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=30),
         'iat':  datetime.datetime.utcnow()
-    },'access_secret',algorithm='HS256')
+    },settings.ACCESS_TOKEN_SECRET,algorithm='HS256')
     
 def decode_access_token(token):
     try:
-        payload = jwt.decode(token,'access_secret',algorithms='HS256')
+        payload = jwt.decode(token,settings.ACCESS_TOKEN_SECRET,algorithms='HS256')
         return payload['user_id']
     except:
         raise exceptions.AuthenticationFailed('unathenticated')
@@ -34,11 +35,11 @@ def create_refresh_token(id):
         'user_id':id,
         'exp': datetime.datetime.utcnow() + datetime.timedelta(days=7),
         'iat':  datetime.datetime.utcnow()
-    },'refresh_secret',algorithm='HS256')
+    },settings.REFRESH_TOKEN_SECRET,algorithm='HS256')
     
 def decode_refresh_token(token):
     try:
-        payload = jwt.decode(token,'refresh_secret',algorithms='HS256')
+        payload = jwt.decode(token,settings.REFRESH_TOKEN_SECRET,algorithms='HS256')
         return payload['user_id']
     except Exception as e:
         print(e)
