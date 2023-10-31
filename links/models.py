9787abd6f4d21d6acd788846f0947bb93459c330
Hesam Haghpanah <43572130+h-haghpanah@users.noreply.github.com>
2023-10-31 10:31:22 +0330

    add model LinkProvider to links application

--- links/models.py
+++ links/models.py
@@ -1,13 +1,20 @@
 from django.db import models
-
+from authentication.models import User
 # Create your models here.
 
 
-class ProvidedLinks(models.Model):
+class Link(models.Model):
     url = models.CharField(max_length=255,null=False,blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     comment = models.CharField(max_length=255,blank=True,default=None)
-    dr = models.SmallIntegerField()
-    traffic = models.BigIntegerField()
-    
+    dr = models.SmallIntegerField(default=0)
+    traffic = models.BigIntegerField(default=0)
+
+class LinkProvider(models.Model):
+    provider = models.ForeignKey(User,on_delete=models.CASCADE)
+    link = models.ForeignKey(Link,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True,null=False,blank=False)
+    price = models.BigIntegerField(default=0)
