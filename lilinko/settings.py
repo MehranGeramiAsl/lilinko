6c500f09fbe20faac95b368867b49dc0a8ef9935
Hesam Haghpanah <43572130+h-haghpanah@users.noreply.github.com>
2023-10-31 10:20:41 +0330

    add model to links app (ProvidedLinks)

--- lilinko/settings.py
+++ lilinko/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'authentication',
+    'links',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- links/models.py
+++ links/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ProvidedLinks(models.Model):
+    url = models.CharField(max_length=255,null=False,blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    comment = models.CharField(max_length=255,blank=True,default=None)
+    dr = models.SmallIntegerField()
+    traffic = models.BigIntegerField()
+    
