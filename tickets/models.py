0346abbddae3868294150135c83dc9efb7a5618c
Hesam Haghpanah <43572130+h-haghpanah@users.noreply.github.com>
2023-11-18 18:12:11 +0330

    create ticket model

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from authentication.models import User
 
 # Create your models here.
+
+
+class Ticket(models.Model):
+    TICKET_STATUS_CHOICES = [
+    ("O", "Open"),
+    ("OP", "On Progress"),
+    ("A", "Answered"),
+    ("C", "Closed"),
+    ]
+
+    title = models.CharField(max_length=255)
+    status = models.CharField(max_length=2,choices=TICKET_STATUS_CHOICES,default="O")
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
+
